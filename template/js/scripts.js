50e0e9ea3ef46420d62b63fa7afd80e23c96f93a
SheilAPK <karaulov.z@mail.ru>
2024-05-13 15:13:35 +0300

    В работе

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
   $(".mask__phone").mask("+7(999) 999-9999");
+
+  const swiper = new Swiper('.price__slider', {
+    // Optional parameters
+    // direction: 'vertical',
+    // loop: true,
+  
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+
+    // Миниатюры
+    thumbs: {
+      swiper: {
+          observer: true,
+          el: '.price__pagination',
+          slidesPerView: 'auto',
+          spaceBetween: 10,
+      }
+    }
+  });
 });
